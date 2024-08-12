bb5e3323784824d7303022573b36ab08a341a0dd
Baktiyar <bahtiyar_0194@mail.ru>
2024-08-12 12:53:29 +0500

    nineteen commit

--- plugins/peer.client.js
+++ plugins/peer.client.js
@@ -6,7 +6,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     
     const peer = new Peer(undefined, {
         host: config.public.peerBase,
-        port: process.env.NODE_ENV === 'development' ? 3002 : null,
+        port: process.env.NODE_ENV === 'development' ? 3002 : '',
         path: '/peerjs/myapp',
         secure: process.env.NODE_ENV === 'development' ? false : true
     });
