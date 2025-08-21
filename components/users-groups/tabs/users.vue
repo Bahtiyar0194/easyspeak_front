134235f33983ae9e2ad8a1cf1c87c5b30cd2f6ea
Baktiyar <bahtiyar_0194@mail.ru>
2025-08-21 16:26:10 +0500

    one hundred ten commit

--- components/users-groups/tabs/users.vue
+++ components/users-groups/tabs/users.vue
@@ -2,15 +2,18 @@
   <div class="custom-grid">
     <div class="col-span-12">
       <div class="btn-wrap">
-        <roleProvider :roles="[1, 2, 3]">
-          <button
-            class="btn btn-outline-primary"
-            @click="inviteModalIsVisible = true"
-          >
-            <i class="pi pi-user-plus"></i>
-            {{ $t("invite") }}
-          </button>
-        </roleProvider>
+        <client-only>
+          <roleProvider :roles="[1, 2, 3]">
+            <button
+              class="btn btn-outline-primary"
+              @click="inviteModalIsVisible = true"
+            >
+              <i class="pi pi-user-plus"></i>
+              {{ $t("invite") }}
+            </button>
+          </roleProvider>
+        </client-only>
+
         <button @click="showHideUserSearchFilter" class="btn btn-light">
           <i class="pi pi-search"></i>
           {{
