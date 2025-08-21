840c8479d50514a8b5a9ba9b7ee57ea1ac03939a
Baktiyar <bahtiyar_0194@mail.ru>
2025-08-21 15:04:45 +0500

    one hundred six commit

--- components/ui/selectLocale.vue
+++ components/ui/selectLocale.vue
@@ -1,54 +1,55 @@
 <template>
-    <dropdownMenu :dropdown-arrow="true">
-        <template v-slot:btn_content>
-            {{ localeProperties.short_name }}
-        </template>
+  <dropdownMenu :dropdown-arrow="true">
+    <template v-slot:btn_content>
+      {{ localeProperties.short_name }}
+    </template>
 
-        <template v-slot:menu_content>
-            <li v-for="l in locales" :key="l.code">
-                <button @click="changeLocale(l.code)">
-                    {{ l.name }}
-                </button>
-            </li>
-        </template>
-    </dropdownMenu>
+    <template v-slot:menu_content>
+      <li v-for="l in locales" :key="l.code">
+        <button @click="changeLocale(l.code)">
+          {{ l.name }}
+        </button>
+      </li>
+    </template>
+  </dropdownMenu>
 </template>
 <script setup>
-import { useNuxtApp } from 'nuxt/app';
-import dropdownMenu from './dropdownMenu';
-import { useRouter } from 'nuxt/app';
+import { useNuxtApp } from "nuxt/app";
+import dropdownMenu from "./dropdownMenu";
+import { useRouter } from "nuxt/app";
 
-
-const { localeProperties, locales, setLocale, } = useI18n();
+const { localeProperties, locales, setLocale } = useI18n();
 const authUser = useSanctumUser();
 const router = useRouter();
 //const { refreshIdentity } = useSanctumAuth();
 const { $axiosPlugin } = useNuxtApp();
 
 const changeLocale = async (value) => {
-    setLocale(value);
+  setLocale(value);
 
-    if (authUser.value) {
-        await $axiosPlugin.post('auth/change_language/' + value)
-            .then(response => {
-                window.location.reload();
-                console.log(123)
-                //refreshIdentity();
-            }).catch(err => {
-                if (err.response) {
-                    router.push({
-                        path: '/error',
-                        query: {
-                            status: err.response.status,
-                            message: err.response.data.message,
-                            url: err.request.responseURL,
-                        }
-                    });
-                }
-                else {
-                    router.push('/error');
-                }
-            });
-    }
-}
+  if (authUser.value) {
+    await $axiosPlugin
+      .post("auth/change_language/" + value)
+      .then((response) => {
+        window.location.reload();
+        //refreshIdentity();
+      })
+      .catch((err) => {
+        if (err.response) {
+          router.push({
+            path: "/error",
+            query: {
+              status: err.response.status,
+              message: err.response.data.message,
+              url: err.request.responseURL,
+            },
+          });
+        } else {
+          router.push("/error");
+        }
+      });
+  } else {
+    window.location.reload();
+  }
+};
 </script>
